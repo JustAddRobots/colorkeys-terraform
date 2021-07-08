539899662f5775a462ca748c3f0752604cfeb5d4
Roderick Constance <justaddrobots@icloud.com>
2021-07-07 18:03:39 -0700

    ISSUE-017: Commented out EventBridge rules

--- infrastructure/service/pipeline/notification.tf
+++ infrastructure/service/pipeline/notification.tf
@@ -17,37 +17,40 @@ resource "aws_codestarnotifications_notification_rule" "stage_colorkeys_pipeline
   }
 }
 
-resource "aws_cloudwatch_event_rule" "stage_colorkeys_pipeline" {
-  name            = "stage-colorkeys-pipeline"
-  description     = "CodePipeline Status Event"
-  event_bus_name  = "default"
-  tags            = var.default_tags
-
-  event_pattern = <<EOF
-{
-  "source": [
-    "aws.codepipeline"
-  ],
-  "detail-type": [
-    "CodePipeline Pipeline Execution State Change"
-  ],
-  "detail": {
-    "state": [
-      "FAILED",
-      "CANCELED",
-      "STARTED",
-      "RESUMED",
-      "STOPPED",
-      "SUCCEEDED",
-      "SUPERSEDED"
-    ]
-  }
-}
-EOF
-}
-
-resource "aws_cloudwatch_event_target" "stage_colorkeys_pipeline" {
-  rule      = "${aws_cloudwatch_event_rule.stage_colorkeys_pipeline.name}"
-  target_id = "SendToSNS"
-  arn       = "${aws_sns_topic.stage_colorkeys_pipeline.arn}"
-}
+# resource "aws_cloudwatch_event_rule" "stage_colorkeys_pipeline" {
+#   name            = "stage-colorkeys-pipeline"
+#   description     = "CodePipeline Status Event"
+#   event_bus_name  = "default"
+#   tags            = var.default_tags
+# 
+#   event_pattern = <<EOF
+# {
+#   "source": [
+#     "aws.codepipeline"
+#   ],
+#   "detail-type": [
+#     "CodePipeline Pipeline Execution State Change"
+#   ],
+#   "detail": {
+#     "state": [
+#       "FAILED",
+#       "CANCELED",
+#       "STARTED",
+#       "RESUMED",
+#       "STOPPED",
+#       "SUCCEEDED",
+#       "SUPERSEDED"
+#     ]
+#   }
+# }
+# EOF
+# }
+# 
+# resource "aws_cloudwatch_event_target" "stage_colorkeys_pipeline" {
+#   rule      = "${aws_cloudwatch_event_rule.stage_colorkeys_pipeline.name}"
+#   target_id = "SendToSNS"
+#   arn       = "${aws_sns_topic.stage_colorkeys_pipeline.arn}"
+#   sqs_target {
+#     message_group_id = "stage-colorkeys"
+#   }
+# }
