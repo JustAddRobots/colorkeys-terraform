ff4cf154324ac0c079b0c09d4456e9111c79af43
Roderick Constance <justaddrobots@icloud.com>
2021-07-09 23:34:20 -0700

    ISSUE-018: Updated slack status to include more GitHub info

--- infrastructure/service/status/stage-colorkeys-status-lambda-slack.py
+++ infrastructure/service/status/stage-colorkeys-status-lambda-slack.py
@@ -1,6 +1,7 @@
 """
     This lambda parses CodePipeline notifications (via SQS) and posts to Slack API.
 """
+import boto3
 import json
 import logging
 import os
@@ -32,16 +33,20 @@ def create_payload(notification):
             "White_on_White_%28Malevich%2C_1918%29.png/"
             "240px-White_on_White_%28Malevich%2C_1918%29.png"
         ),
-        "RESUMED": "https://upload.wikimedia.org/wikipedia/en/7/74/PicassoGuernica.jpg",
+        "RESUMED": (
+            "https://upload.wikimedia.org/wikipedia/en/7/74/PicassoGuernica.jpg"
+        ),
         "SUCCEEDED": (
             "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/"
             "Klimt_-_Orchard_with_Roses.jpg/240px-Klimt_-_Orchard_with_Roses.jpg"
         ),
         "FAILED": (
-            "https://upload.wikimedia.org/wikipedia/en/c/cc/Atelier_rouge_matisse_1.jpg"
+            "https://upload.wikimedia.org/wikipedia/en/c/cc/"
+            "Atelier_rouge_matisse_1.jpg"
         ),
         "CANCELED": (
-            "https://upload.wikimedia.org/wikipedia/en/c/cc/Atelier_rouge_matisse_1.jpg"
+            "https://upload.wikimedia.org/wikipedia/en/c/cc/"
+            "Atelier_rouge_matisse_1.jpg"
         ),
         "SUPERSEDED": (
             "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/"
@@ -59,13 +64,15 @@ def create_payload(notification):
 
     message_text = (
         f"*{notification['state']}* {notification['pipeline']} "
-        f"{notification['exec_id'][:8]} <{pipeline_url} | (Open)> "
-        f"{notification['timestamp']}"
+        f"<{pipeline_url} | ({notification['exec_id'][:8]})> "
+        f"{notification['timestamp']}\n"
+        f"<{notification['commit_url']} | {notification['commit_id'][:8]}> "
+        f"{notification['commit_msg']}"
     )
 
     # Add action and reason for failed state
     list_ = []
-    failure_text = " "
+    failure_text = " "  # Slack fails on empty text string
     if notification["state"].lower() == "failed":
         for i in notification["failures"]:
             list_.append(f"{i['action']}: {i['additionalInformation']}")
@@ -119,18 +126,34 @@ def post_status(url, payload):
     return res
 
 
-def parse_sqs_status(event):
-    """Parse SQS event.
+def get_rev(pipeline, exec_id):
+    """Get GitHub artifact revision from CopePipeline execution.
 
-    SQS event is subscribed to SNS topic.
+    Args:
+        pipeline (str): Pipeline name.
+        exec_id (str): Pipeline execution ID.
+
+    Returns:
+        rev (dict): Artifact revision for pipeline execution.
+    """
+    cp = boto3.client("codepipeline")
+    r = cp.get_pipeline_execution(
+        pipelineName = pipeline,
+        pipelineExecutionId = exec_id
+    )
+    rev = next(i for i in r["pipelineExecution"]["artifactRevisions"])
+    return rev
+
+
+def get_notification(event):
+    """Get notification info from SQS event.
 
     Args:
         event (SQS message event): CodePipeline notificataion piped through SNS.
 
     Returns:
-        response (str): HTTP response.
+        notification (dict): Summary of CodePipeline notification.
     """
-    # extract CodePipeline notification
     record = next(i for i in event["Records"])
     record_body = json.loads(record["body"])
     ev = json.loads(record_body["Message"])
@@ -142,9 +165,28 @@ def parse_sqs_status(event):
     notification["region"] = record["awsRegion"]
     if notification["state"].lower() == "failed":
         notification["failures"] = ev["additionalAttributes"]["failedActions"]
+    rev = get_rev(notification["pipeline"], notification["exec_id"])
+    notification["commit_id"] = rev["revisionId"]
+    notification["commit_url"] = rev["revisionUrl"]
+    notification["commit_msg"] = json.loads(
+        rev["revisionSummary"]
+    )["CommitMessage"]
     logger.info(f"notification: {notification}")
+    return notification
 
-    # create payload and url for GitHub status update
+
+def post_sqs_status(event):
+    """Parse SQS event and post status to Slack API.
+
+    SQS event is subscribed to SNS topic.
+
+    Args:
+        event (SQS message event): CodePipeline notificataion piped through SNS.
+
+    Returns:
+        response (str): HTTP response.
+    """
+    notification = get_notification(event)
     payload = create_payload(notification)
     SLACK_URL_SUFFIX = os.getenv("SLACK_URL_SUFFIX")
     url = f"https://hooks.slack.com/services/{SLACK_URL_SUFFIX}"
@@ -159,7 +201,7 @@ def parse_sqs_status(event):
 def lambda_handler(event, context):
     """Handle CodePipeline Notification in SQS event."""
     try:
-        resp = parse_sqs_status(event)
+        resp = post_sqs_status(event)
     except Exception as exc:
         logger.info("Lambda Failure")
         logger.info(f"event: {event}")
